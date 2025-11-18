b0f6bbbbd70c395d77b8725997086020375658d1
Stone Olguin <olguinstone@gmail.com>
2025-11-17 22:31:44 -0700

    Add point logic cleanup

--- src/utils/point.rs
+++ src/utils/point.rs
@@ -1,104 +1,108 @@
 use ndarray::Array2;
 
-#[derive(PartialEq, Eq, Debug)]
+#[derive(PartialEq, Eq, Debug, Clone, Copy)]
 pub struct Point<'a> {
-  grid: &'a Array2<char>,
-  x: usize,
-  y: usize,
+    grid: &'a Array2<char>,
+    x: usize,
+    y: usize,
 }
 
-// impl<'a> Default for Point<'a> {
-//     fn default() -> Self {
-//         Self { grid: Default::default(), x: Default::default(), y: Default::default() }
-//     }
-// }
-
 impl<'a> Point<'a> {
-  pub fn new(grid: &'a Array2<char>, initial_x: usize, initial_y: usize) -> Self  {
-    Self { grid, x: initial_x, y: initial_y }
-  }
-
-  pub fn left(&self) -> Option<Point<'_>> {
-    match self.x == 0 {
-        true => None,
-        false => Some(Point{grid: self.grid, x: self.x-1, y: self.y}),
+    pub fn new(grid: &'a Array2<char>, initial_x: usize, initial_y: usize) -> Self {
+        Self {
+            grid,
+            x: initial_x,
+            y: initial_y,
+        }
     }
-  }
 
-  pub fn right(&self) -> Option<Point<'_>> {
-    match self.x >= self.grid.dim().0 - 1 {
-        true => None,
-        false => Some(Point{grid: self.grid, x: self.x+1, y: self.y}),
-    }
-  }
-
-  pub fn down(&self) -> Option<Point<'_>> {
-    match self.y >= self.grid.dim().1 - 1 {
-        true => None,
-        false => Some(Point{grid: self.grid, x: self.x, y: self.y+1}),
-    }
-  }
-
-  pub fn up(&self) -> Option<Point<'_>> {
-    match self.y == 0 {
-        true => None,
-        false => Some(Point{grid: self.grid, x: self.x, y: self.y-1}),
-    }
-  }
-
-  pub fn value(&self) -> Option<&char> {
-    self.grid.get((self.x, self.y))
-  }
-
-  pub fn neighbors(&self) -> Array2<Option<&char>> {
-    let directive_grid = vec![
-      vec![
-        vec![Point::up, Point::left],
-        vec![Point::up],
-        vec![Point::up, Point::right],
-      ],
-      vec![vec![Point::left], vec![], vec![Point::right]],
-      vec![
-        vec![Point::down, Point::left],
-        vec![Point::down],
-        vec![Point::down, Point::right],
-      ],
-    ];
-
-    let rows = directive_grid.len();
-    let cols = directive_grid.first().map(|row| row.len()).unwrap_or(0);
-    let mut neighbors = Array2::from_elem((rows, cols), None);
-
-    // TODO: THIS IS TRANSPOSED SOMEHOW
-    for (row_idx, row) in directive_grid.iter().enumerate() {
-      for (col_idx, chain) in row.iter().enumerate() {
-        let final_value = chain.iter().fold(
-          Some((self.x, self.y)),
-          |acc, directive| {
-            acc.and_then(|(x, y)| {
-              let point = Point {
+    pub fn left(&self) -> Option<Point<'_>> {
+        match self.x == 0 {
+            true => None,
+            false => Some(Point {
                 grid: self.grid,
-                x,
-                y,
-              };
-              directive(&point).map(|next| (next.x, next.y))
-            })
-          },
-        ).and_then(|(x, y)| self.grid.get((x, y)));
-
-        neighbors[(row_idx, col_idx)] = final_value;
-      }
+                x: self.x - 1,
+                y: self.y,
+            }),
+        }
     }
 
-    neighbors
-  }
+    pub fn right(&self) -> Option<Point<'_>> {
+        match self.x >= self.grid.dim().0 - 1 {
+            true => None,
+            false => Some(Point {
+                grid: self.grid,
+                x: self.x + 1,
+                y: self.y,
+            }),
+        }
+    }
 
+    pub fn down(&self) -> Option<Point<'_>> {
+        match self.y >= self.grid.dim().1 - 1 {
+            true => None,
+            false => Some(Point {
+                grid: self.grid,
+                x: self.x,
+                y: self.y + 1,
+            }),
+        }
+    }
+
+    pub fn up(&self) -> Option<Point<'_>> {
+        match self.y == 0 {
+            true => None,
+            false => Some(Point {
+                grid: self.grid,
+                x: self.x,
+                y: self.y - 1,
+            }),
+        }
+    }
+
+    pub fn value(&self) -> Option<&char> {
+        self.grid.get((self.x, self.y))
+    }
+
+    pub fn neighbors(&self) -> Array2<Option<&char>> {
+        let directive_grid = vec![
+            vec![Point::up, Point::left],
+            vec![Point::up],
+            vec![Point::up, Point::right],
+            vec![Point::left],
+            vec![],
+            vec![Point::right],
+            vec![Point::down, Point::left],
+            vec![Point::down],
+            vec![Point::down, Point::right],
+        ];
+
+        let map: Vec<Option<&char>> = directive_grid
+            .iter()
+            .map(|directives| {
+                directives
+                    .iter()
+                    .fold(Some(self.clone()), |acc, directive| {
+                        acc.and_then(|p| {
+                            directive(&p).map(|next| Point::new(self.grid, next.x, next.y))
+                        })
+                    })
+            })
+            .map(|maybe_point| maybe_point.and_then(|point| self.grid.get((point.x, point.y))))
+            .collect();
+
+        let (rows, cols) = (3, 3);
+        match Array2::from_shape_vec((rows, cols), map) {
+            Ok(a) => a.reversed_axes(),
+            Err(_) => Array2::from_elem((rows, cols), None),
+        }
+    }
 }
 
 #[cfg(test)]
 pub mod point_test {
-    use crate::utils::point::Point;
     use crate::utils::parse_input_data::parse_input_to_grid;
+    use crate::utils::point::Point;
 
     const EXAMPLE_INPUT: &str = r#"
     1234567890
@@ -106,7 +110,6 @@ pub mod point_test {
     1234567890
     1234567890
     "#;
-
 
     #[test]
     fn test_value() {
@@ -166,22 +169,21 @@ pub mod point_test {
         let grid = parse_input_to_grid(EXAMPLE_INPUT);
         let point = Point::new(&grid, 1, 1);
         let neighbors = point.neighbors();
-        println!("{:?}", neighbors);
 
         let expectations = [
-            ((0, 0), (0, 0)),
-            ((0, 1), (1, 0)),
-            ((0, 2), (2, 0)),
-            ((1, 0), (0, 1)),
-            ((1, 1), (1, 1)),
-            ((1, 2), (2, 1)),
-            ((2, 0), (0, 2)),
-            ((2, 1), (1, 2)),
-            ((2, 2), (2, 2)),
+            (0, 0),
+            (0, 1),
+            (0, 2),
+            (1, 0),
+            (1, 1),
+            (1, 2),
+            (2, 0),
+            (2, 1),
+            (2, 2),
         ];
 
-        for (matrix_coord, grid_coord) in expectations {
-            assert_eq!(neighbors[matrix_coord], grid.get(grid_coord));
+        for (x, y) in expectations {
+            assert_eq!(neighbors[(x, y)], grid.get((x, y)));
         }
 
         let corner_point = Point::new(&grid, 0, 0);
