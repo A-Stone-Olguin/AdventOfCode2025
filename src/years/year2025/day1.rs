8ac3f4e793a0373cb504df732083dfe15916822b
Stone Olguin <olguinstone@gmail.com>
2025-11-30 23:40:11 -0700

    Cleanup of day 1

--- src/years/year2025/day1.rs
+++ src/years/year2025/day1.rs
@@ -15,62 +15,18 @@ fn parse_turns(input: &str) -> Vec<(Turn, i32)> {
     let regex = Regex::new(r"(L|R)(\d+)").unwrap();
     let captures = parse_input_data::captures_per_row(rows, &regex);
 
-    let mut vec: Vec<(Turn, i32)> = vec![];
-    for capture in captures {
-        let inserted_value = match capture.get(0).unwrap().get(0).unwrap().as_str() {
-            "L" => Turn::Left,
-            "R" => Turn::Right,
-            _ => panic!("Got invalid value"),
-        };
-        let turn_amount: i32 = capture.get(0).unwrap().get(1).unwrap().parse().unwrap();
-        vec.push((inserted_value, turn_amount));
-    }
-    vec
-}
-
-fn calculate_turns(turns: &[(Turn, i32)], initial_value: i32) -> i32 {
-    let mut zero_count = 0;
-    let mut current_value = initial_value;
-    for (direction, clicks) in turns {
-        let change_value: i32 = match direction {
-            Turn::Left => -1 * clicks,
-            Turn::Right => *clicks,
-        };
-        current_value = (current_value + change_value) % 100;
-        if current_value == 0 {
-            zero_count += 1;
-        }
-    }
-    zero_count
-}
-
-fn count_zero_occurrences(turns: &[(Turn, i32)], initial_value: i32) -> i32 {
-    let mut zero_count = 0;
-    let mut current_value = initial_value;
-    for (direction, clicks) in turns {
-        let count_threshold: i32 = match direction {
-            Turn::Left => current_value,
-            Turn::Right => 100 - current_value,
-        };
-
-        // Any "cycles" around
-        let cycles = clicks / 100;
-        zero_count += cycles;
-
-        // Get any "intermediate clicks", don't count 0 twice
-        if *clicks - cycles * 100 >= count_threshold && count_threshold != 0 {
-            zero_count += 1;
-        }
-
-        // TODO CLeanup and remove
-        let change_value: i32 = match direction {
-            Turn::Left => -1 * clicks,
-            Turn::Right => *clicks,
-        };
-        current_value = (current_value + change_value).rem_euclid(100);
-    }
-
-    zero_count
+    captures
+        .iter()
+        .map(|capture| {
+            let inserted_value = match capture.get(0).unwrap().get(0).unwrap().as_str() {
+                "L" => Turn::Left,
+                "R" => Turn::Right,
+                _ => panic!("Got invalid value"),
+            };
+            let turn_amount: i32 = capture.get(0).unwrap().get(1).unwrap().parse().unwrap();
+            (inserted_value, turn_amount)
+        })
+        .collect()
 }
 
 fn turn_amount(current_value: i32, (direction, clicks): &(Turn, i32)) -> i32 {
